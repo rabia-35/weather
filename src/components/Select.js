0fc13cd418bea967ab76c562a02954db54f29247
rabiaolker <85495654+rabia-35@users.noreply.github.com>
2022-04-11 18:09:40 +0300

    Update Select.js
    handleSubmit function eklendi.

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,9 +3,13 @@ import { CityContext } from '../contexts/City'
 
 function Select() {
     const {city, setCity}= useContext(CityContext)// city.js dosyasından city ve setCity değerlerini çekme
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        setCity(e.target.value)
+    }
     return (
         <div >
-            <input placeholder='şehir adı giriniz.' className="input" name="city" value={city} onChange={(e)=>setCity(e.target.value)}/>
+            <input placeholder='şehir adı giriniz.' className="input" name="city" value={city} onChange={handleSubmit}/>
         </div>
     )
 }
