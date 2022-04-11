a280c9904b27eb02b375373503ae421eaf8a58ea
rabia ölker <rabia.olker@gmail.com>
2022-04-11 19:27:37 +0300

    select.js dosyasına handleSubmit function eklendi.

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,17 +1,19 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { CityContext } from '../contexts/City'
 
 function Select() {
-    const {city, setCity}= useContext(CityContext)// city.js dosyasından city ve setCity değerlerini çekme
+    const [title, setTitle]=useState()
+    const { setCity}= useContext(CityContext)// city.js dosyasından city ve setCity değerlerini çekme
     const handleSubmit=(e)=>{
         e.preventDefault();
-        setCity(e.target.value)
-        setCity("");
+        setCity(title)
+        setTitle("");
     }
     return (
-        <div >
-            <input placeholder='şehir adı giriniz.' className="input" name="city" value={city} onChange={handleSubmit()}/>
-        </div>
+        <form onSubmit={handleSubmit} >
+            <input placeholder='şehir adı giriniz.' className="input" name="city" value={title} onChange={(e)=>setTitle(e.target.value)} />
+        </form>
+           
     )
 }
 
