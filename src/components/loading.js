a4af60f62ff6ecd48d86822c8131b32c826e29e7
rabia <rabia.olker@gmail.com>
2022-08-23 16:14:03 +0300

    created loading file

---
+++ src/components/loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div className='text-center'>
+      <div class="spinner-border text-light" role="status">
+         <span class="visually-hidden">Loading...</span>
+    </div>
+    </div>
+  )
+}
+
+export default Loading
