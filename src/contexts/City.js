55c4dfa26f887914e8735a077a19528488c6ea09
rabia <rabia.olker@gmail.com>
2022-08-23 16:14:32 +0300

    created city context file

--- src/contexts/City.js
+++ src/contexts/City.js
@@ -1,12 +1,19 @@
 import React, { createContext, useState } from "react";
+import cityData from "../cityData.json"
+
+
 export const CityContext=createContext();
 
-export const CityProvider=({ children })=>{
+
+const CityProvider=({ children })=>{
     const [city, setCity] = useState("izmir") 
-    const values={city, setCity}
+    const cities=[...cityData]
+    const values={city, setCity, cities}
     
     return (
         <CityContext.Provider value={values}>{ children }</CityContext.Provider>
        
     )
 }
+
+export default CityProvider;
