32f5b54382c992149761d37b6b51981435745387
rabia <rabia.olker@gmail.com>
2022-08-23 16:16:00 +0300

    added dropdown for city

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,18 +1,26 @@
 import { useContext, useState } from 'react'
 import { CityContext } from '../contexts/City'
 
+
 function Select() {
-    const [title, setTitle]=useState()
-    const { setCity}= useContext(CityContext)// city.js dosyasından city ve setCity değerlerini çekme
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        setCity(title)
-        setTitle("");
-    }
+    const {city, setCity, cities}= useContext(CityContext)// city.js dosyasından city ve setCity değerlerini çekme
+  console.log(city)
     return (
-        <form onSubmit={handleSubmit} >
-            <input placeholder='şehir adı giriniz.' className="input" name="city" value={title} onChange={(e)=>setTitle(e.target.value)} />
-        </form>
+       <div className=' selected py-5'>
+         <div className="btn-group">
+            <button className="btn btn-outline-warning btn-lg dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cities of Turkey
+            </button>
+            <ul className="dropdown-menu">
+                {cities &&(
+                    cities.map(city=>(
+                        <li className='dropdown-item' key={city.id}  onClick={()=>setCity(city.name)} >{city.name}</li>
+                    ))
+                )
+                }
+            </ul>
+        </div>
+       </div>
            
     )
 }
