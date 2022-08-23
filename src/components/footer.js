eb9e29c5af432f5875ec5243124b8e7ded6d845c
rabia <rabia.olker@gmail.com>
2022-08-23 16:13:42 +0300

    added creator's github link and her name to footer file

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+
+   
+function Footer() {
+  return (
+    <div className='footer text-center mt-5'>
+        <a href='https://github.com/rabia-35/memory-game'>
+        <FontAwesomeIcon icon={faGithub} className="fa-3x " />
+        </a>  
+        <p className=' pb-3 mb-0 text-light'> Created by Rabia ÖLKER</p>
+    </div>
+  )
+}
+
+export default Footer
