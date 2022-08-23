0df0d464747c6adc6ce25c77f322ac29d8b0cce3
rabia <rabia.olker@gmail.com>
2022-08-23 16:11:39 +0300

    imported files to appjs file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import  { Weather } from './contexts/WeatherData'
-
 import './App.css';
-import { CityProvider } from './contexts/City';
+import WeatherPro  from './contexts/Weather'
+import CityProvider  from './contexts/City';
 import Select from './components/Select';
+import WeatherCard from './components/weatherCard';
+import Footer from "./components/footer"
 
 function App() {
   
   return (
-    <div >
-     <CityProvider>
-     <Select />
-        <Weather />
-        
+   <div className='App' style={{ backgroundImage: `url(/images/cover.jpg)` }}  >
+       <CityProvider>
+        <WeatherPro>
+          <Select />
+          <WeatherCard />
+          <Footer />
+        </WeatherPro> 
       </CityProvider>
+   </div>
+    
       
-    </div>
+    
   );
 }
 
